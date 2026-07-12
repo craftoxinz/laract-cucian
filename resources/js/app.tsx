d6668946b5930f0c5be156ac19052a799b3834ee
craftoxinz <winnestkyouka12@gmail.com>
2026-07-13 00:53:45 +0700

    chore: enable React strictMode for better development debugging

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    strictMode: true,
     progress: {
         color: '#4B5563',
     },
